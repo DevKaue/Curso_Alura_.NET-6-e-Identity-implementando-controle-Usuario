6bca2909cdeb05671e68a1e86c5de76aafcefac9
Kauê Wendt Sabino <kaue.sabino@modalgr.com.br>
2024-03-26 13:13:47 -0300

    Criando o Usuario

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddAutoMapper
+    (AppDomain.CurrentDomain.GetAssemblies());
 // Add services to the container.
 
 builder.Services.AddControllers();
